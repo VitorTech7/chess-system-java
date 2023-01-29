b9100e9001a2a4f92e8526dd780a2d698f95e58b
Vitor Alberto <vytroxalberto@gmail.com>
2023-01-29 14:13:15 -0300

    Special move - Castling

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,14 +1,18 @@
 package chess.pieces;
 
+
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece{
+	
+	
 
 	public Pawn(Board board, Color color) {
 		super(board, color);
+		
 
 	}
 
@@ -71,3 +75,20 @@ public class Pawn extends ChessPiece{
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
